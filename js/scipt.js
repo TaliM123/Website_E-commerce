c298eb40f7ce1d9c582b5f9bf7060a1f3e3e317e
redonbajrami <rb51806@ubt-uni.net>
2021-12-14 21:28:36 +0100

    vazhdim

--- js/scipt.js
+++ js/scipt.js
@@ -6,3 +6,17 @@ $(document).on('click','.search',function(){
 $(document).on('click','.search-cancel',function(){
     $('.search-bar').removeClass('search-bar-active')
 });
+//Funksion per log in dhe sign up 
+$(document).on('click','.user.already-account',function(){
+    $('.form').addClass('login-active').removeClass('login-active')
+});
+
+
+$(document).on('click','.sign-up-btn',function(){
+    $('.form').addClass('sign-up-active').removeClass('login-active')
+});
+
+
+$(document).on('click','.form-cancel',function(){
+    $('.form').removeClass('login-active').removeClass('sign-up-active')
+});
