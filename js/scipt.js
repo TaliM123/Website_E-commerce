818180d4d382f8724da2794923db1217f4df2b23
Taulant Merlaku <93524968+TaliM123@users.noreply.github.com>
2021-12-16 22:41:10 +0100

    Removed Automatic slider - Made a Manual one
    E kom hek at sliderin e gatshem e qita njo tjeter ma te thjesht

--- js/scipt.js
+++ js/scipt.js
@@ -19,27 +19,34 @@ $(document).on('click', '.form-cancel', function() {
     $('.form').removeClass('login-active').removeClass('sign-up-active')
 });
 
-//Full slider - slider per ofeta 
-$(document).ready(function() {
-    $('#adaptive').lightSlider({
-        adaptiveHeight: true,
-        auto: true,
-        item: 1,
-        slideMargin: 0,
-        loop: true
-    });
-});
-
 //Feature Slider - Slideri per Maicat ose produktet tjera qe jan ne dispozicion
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth: true,
-        loop: true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        }
-    });
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("full-slider-box");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
 
 // Per fix menu gjate levizjes ne faqe rrin gjithmon ne top dhe kur preket ikona e search ose login
 //nuk del ne fillin te faqes pra ndreq kete error
