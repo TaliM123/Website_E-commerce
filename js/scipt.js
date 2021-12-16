ff2c64db2d2f08de70e9b32768962de80d9e8014
Taulant Merlaku <93524968+TaliM123@users.noreply.github.com>
2021-12-16 22:53:37 +0100

    Bug fix with navigation bar

--- js/scipt.js
+++ js/scipt.js
@@ -19,35 +19,6 @@ $(document).on('click', '.form-cancel', function() {
     $('.form').removeClass('login-active').removeClass('sign-up-active')
 });
 
-//Feature Slider - Slideri per Maicat ose produktet tjera qe jan ne dispozicion
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("full-slider-box");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
 // Per fix menu gjate levizjes ne faqe rrin gjithmon ne top dhe kur preket ikona e search ose login
 //nuk del ne fillin te faqes pra ndreq kete error
 $(window).scroll(function() {
